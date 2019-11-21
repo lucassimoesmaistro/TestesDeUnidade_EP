6526d9e166d9cef67d585949f7be42092bdadc75
Lucas Simões <lucassimoes_@hotmail.com>
2019-11-21 08:39:22 -0300

    Final Aula Pedido - Command Handler - Parte 1

---
+++ 02 - TDD/src/NerdStore.Vendas.Domain/IPedidoRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Vendas.Domain
+{
+    public interface IPedidoRepository // : IRepository<Pedido>
+    {
+        void Adicionar(Pedido pedido);
+        void Atualizar(Pedido pedido);
+        Task<Pedido> ObterPedidoRascunhoPorClienteId(Guid clienteId);
+        void AdicionarItem(PedidoItem pedidoItem);
+        void AtualizarItem(PedidoItem pedidoItem);
+    }
+}
