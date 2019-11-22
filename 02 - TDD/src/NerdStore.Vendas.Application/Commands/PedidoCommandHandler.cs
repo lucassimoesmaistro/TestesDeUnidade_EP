25eeda002b979d1331acce542e231d12fa42b0e3
Lucas Simões <lucassimoes_@hotmail.com>
2019-11-22 09:22:33 -0300

    Final Aula Pedido - Command Handler - Parte 3

--- 02 - TDD/src/NerdStore.Vendas.Application/Commands/PedidoCommandHandler.cs
+++ 02 - TDD/src/NerdStore.Vendas.Application/Commands/PedidoCommandHandler.cs
@@ -1,6 +1,9 @@
-﻿using System.Threading;
+﻿using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using NerdStore.Core.DomainObjects;
+using NerdStore.Core.Messages;
 using NerdStore.Vendas.Application.Events;
 using NerdStore.Vendas.Domain;
 
@@ -20,51 +23,50 @@ namespace NerdStore.Vendas.Application.Commands
 
         public async Task<bool> Handle(AdicionarItemPedidoCommand message, CancellationToken cancellationToken)
         {
+            if (!ValidarComando(message)) return false;
+
+            var pedido = await _pedidoRepository.ObterPedidoRascunhoPorClienteId(message.ClienteId);
             var pedidoItem = new PedidoItem(message.ProdutoId, message.Nome, message.Quantidade, message.ValorUnitario);
-            var pedido = Pedido.PedidoFactory.NovoPedidoRascunho(message.ClienteId);
-            pedido.AdicionarItem(pedidoItem);
 
-            _pedidoRepository.Adicionar(pedido);
+            if (pedido == null)
+            {
+                pedido = Pedido.PedidoFactory.NovoPedidoRascunho(message.ClienteId);
+                pedido.AdicionarItem(pedidoItem);
 
+                _pedidoRepository.Adicionar(pedido);
+            }
+            else
+            {
+                var pedidoItemExistente = pedido.PedidoItemExistente(pedidoItem);
+                pedido.AdicionarItem(pedidoItem);
+
+                if (pedidoItemExistente)
+                {
+                    _pedidoRepository.AtualizarItem(pedido.PedidoItems.FirstOrDefault(p => p.ProdutoId == pedidoItem.ProdutoId));
+                }
+                else
+                {
+                    _pedidoRepository.AdicionarItem(pedidoItem);
+                }
+
+                _pedidoRepository.Atualizar(pedido);
+            }
 
             pedido.AdicionarEvento(new PedidoItemAdicionadoEvent(pedido.ClienteId, pedido.Id, message.ProdutoId,
                 message.Nome, message.ValorUnitario, message.Quantidade));
 
             return await _pedidoRepository.UnitOfWork.Commit();
-            //if (!ValidarComando(message)) return false;
-
-            //var pedido = await _pedidoRepository.ObterPedidoRascunhoPorClienteId(message.ClienteId);
-            //var pedidoItem = new PedidoItem(message.ProdutoId, message.Nome, message.Quantidade, message.ValorUnitario);
-
-            //if (pedido == null)
-            //{
-            //    pedido = Pedido.PedidoFactory.NovoPedidoRascunho(message.ClienteId);
-            //    pedido.AdicionarItem(pedidoItem);
-
-            //    _pedidoRepository.Adicionar(pedido);
-            //}
-            //else
-            //{
-            //    var pedidoItemExistente = pedido.PedidoItemExistente(pedidoItem);
-            //    pedido.AdicionarItem(pedidoItem);
-
-            //    if (pedidoItemExistente)
-            //    {
-            //        _pedidoRepository.AtualizarItem(pedido.PedidoItems.FirstOrDefault(p => p.ProdutoId == pedidoItem.ProdutoId));
-            //    }
-            //    else
-            //    {
-            //        _pedidoRepository.AdicionarItem(pedidoItem);
-            //    }
-
-            //    _pedidoRepository.Atualizar(pedido);
-            //}
-
-            //pedido.AdicionarEvento(new PedidoItemAdicionadoEvent(pedido.ClienteId, pedido.Id, message.ProdutoId,
-            //    message.Nome, message.ValorUnitario, message.Quantidade));
-
-            //return await _pedidoRepository.UnitOfWork.Commit();
         }
+        private bool ValidarComando(Command message)
+        {
+            if (message.EhValido()) return true;
 
+            foreach (var error in message.ValidationResult.Errors)
+            {
+                _mediator.Publish(new DomainNotification(message.MessageType, error.ErrorMessage));
+            }
+
+            return false;
+        }
     }
 }
