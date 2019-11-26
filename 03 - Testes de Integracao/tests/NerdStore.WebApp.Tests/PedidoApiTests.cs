30dbf34deaa4e83baac42b14ef2e4cef53d976b6
Lucas Simões <lucassimoes_@hotmail.com>
2019-11-26 08:43:06 -0300

    Final Aula: Testes de Integração - Pedidos - API 2

--- 03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/PedidoApiTests.cs
+++ 03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/PedidoApiTests.cs
@@ -40,5 +40,21 @@ namespace NerdStore.WebApp.Tests
             // Assert
             postResponse.EnsureSuccessStatusCode();
         }
+
+        [Fact(DisplayName = "Remover item em pedido existente"), TestPriority(2)]
+        [Trait("Categoria", "Integração API - Pedido")]
+        public async Task RemoverItem_PedidoExistente_DeveRetornarComSucesso()
+        {
+            // Arrange
+            var produtoId = new Guid("9CAA249A-CD63-4832-A2EC-08D758800A34");
+            await _testsFixture.RealizarLoginApi();
+            _testsFixture.Client.AtribuirToken(_testsFixture.UsuarioToken);
+
+            // Act
+            var deleteResponse = await _testsFixture.Client.DeleteAsync($"api/carrinho/{produtoId}");
+
+            // Assert
+            deleteResponse.EnsureSuccessStatusCode();
+        }
     }
 }
